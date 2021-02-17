65e19df81ee1c28cd1f811bbab52303c29ecccee
Juhani Halkomäki <juhani.halkomaki@robu.fi>
2021-02-17 22:18:00 +0200

    Reorganize translations again

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './translations/i18n';
+import './i18n';
 import App from 'components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/theme.js
@@ -0,0 +1,30 @@
+const theme = {
+  unitSize: 8,
+  colors: {
+    primary: '#88ff88',
+    secondary: '#8888ff',
+    black: '#000000',
+    white: '#ffffff',
+    grey: '#888888',
+  },
+  font: {
+    size: {
+      h1: 40,
+      h2: 25,
+      body: 16,
+      large: 25,
+      small: 12,
+    },
+    weight: {
+      bold: 700,
+      normal: 400,
+      thin: 200,
+    },
+    family: {
+      heading: 'Roboto',
+      body: 'Arial',
+    },
+  },
+};
+
+export default theme;
