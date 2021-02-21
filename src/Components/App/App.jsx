0e27302db88f92a16ed04a8aa2c56f4675e70c19
Juhani Halkomäki <juhani.halkomaki@robu.fi>
2021-02-21 23:19:44 +0200

    Add get stops by radius query

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import TranslationProvider from 'providers/TranslationProvider';
 import Header from 'components/layout/Header';
 import Main from 'components/layout/Main';
@@ -8,15 +9,22 @@ import Footer from 'components/layout/Footer';
 import theme from 'theme';
 import './App.css';
 
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql',
+});
+
 const App = () => (
   <React.Suspense fallback={null}>
     <ThemeProvider theme={theme}>
       <TranslationProvider ns={['default']}>
-        <Router>
-          <Header />
-          <Main />
-          <Footer />
-        </Router>
+        <ApolloProvider client={client}>
+          <Router>
+            <Header />
+            <Main />
+            <Footer />
+          </Router>
+        </ApolloProvider>
       </TranslationProvider>
     </ThemeProvider>
   </React.Suspense>
--- src/pages/Address.jsx
+++ src/pages/Address.jsx
@@ -3,14 +3,40 @@ import useTranslation from 'hooks/useTranslation';
 import useQueryParams from '../hooks/useQueryParams';
 import { H2, Paragraph } from '../components/UI/Text';
 import styled from 'styled-components';
+import { gql, useQuery } from '@apollo/client';
 
 const Container = styled.div(({ full }) => ({
   flex: full ? 1 : 0,
 }));
 
+const GET_STOPS_BY_RADIUS = gql`
+  query GetStopsByRadius($lat: Float!, $lng: Float!, $radius: Int!) {
+    stopsByRadius(lat: $lat, lon: $lng, radius: $radius) {
+      edges {
+        node {
+          stop {
+            gtfsId
+            name
+          }
+          distance
+        }
+      }
+    }
+  }
+`;
+
+const RADIUS = 1000;
+
 const Address = () => {
   const { t } = useTranslation();
   const query = useQueryParams();
+  const lat = parseFloat(query.get('lat'));
+  const lng = parseFloat(query.get('lng'));
+  const { loading, error, data } = useQuery(GET_STOPS_BY_RADIUS, {
+    variables: { lat, lng, radius: RADIUS },
+  });
+
+  console.log({ loading, error, data });
 
   return (
     <Container full>
