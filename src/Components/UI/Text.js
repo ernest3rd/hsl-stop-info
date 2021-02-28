900f65a1739723cfccef651774eab0a3bdb52017
Juhani Halkomäki <juhani.halkomaki@robu.fi>
2021-03-01 01:09:02 +0200

    Add more heading options

--- src/Components/UI/Text.js
+++ src/Components/UI/Text.js
@@ -21,6 +21,20 @@ export const H2 = styled.h2(({ theme }) => ({
   color: theme.colors.body,
 }));
 
+export const H3 = styled.h3(({ theme }) => ({
+  fontFamily: theme.font.family.heading,
+  fontSize: theme.font.size.h3,
+  fontWeight: theme.font.weight.bold,
+  color: theme.colors.body,
+}));
+
+export const H4 = styled.h4(({ theme }) => ({
+  fontFamily: theme.font.family.heading,
+  fontSize: theme.font.size.h4,
+  fontWeight: theme.font.weight.bold,
+  color: theme.colors.body,
+}));
+
 export const Paragraph = styled.p(({ padding, margin }) => ({
   margin,
   padding,
